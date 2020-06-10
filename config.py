5c6d282498d1adc71bf8cdd2216dcc8cc8a412ed
Heorhii Yatsenko <gosha1104@gmail.com>
2020-06-10 12:38:30 +0300

    CCTRI-1204/add sightings mapping logic (#3)
    * added sightings, added client for rest
    
    * added tests
    
    * changed relations logic
    
    * fixed tests mock
    
    * fixed flake
    
    * fixed check limit

--- config.py
+++ config.py
@@ -8,6 +8,43 @@ class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-    URL_SCAN_API_URL = 'https://urlscan.io/api/v1/{endpoint}'
+    URL_SCAN_SOURCE_NAME = 'urlscan.io'
 
-    URL_SCAN_HEALTH_CHECK_PARAM = 'ip:1.1.1.1'
+    URL_SCAN_API_URL = 'https://urlscan.io/api/v1/{endpoint}'
+    URL_SCAN_UI_URL = 'https://urlscan.io/result/{id}'
+
+    URL_SCAN_HEALTH_CHECK_OBSERVABLE = {
+        'type': 'ip',
+        'value': '1.1.1.1'
+    }
+
+    URL_SCAN_OBSERVABLE_TYPES = {
+        'ip': 'ip:{observable}',
+        'ipv6': 'ip:{observable}',
+        'domain': 'domain:{observable}',
+        'url': 'page.url:{observable}'
+    }
+
+    USER_AGENT = ('Cisco Threat Response Integrations '
+                  '<tr-integrations-support@cisco.com>')
+
+    CTR_DEFAULT_ENTITIES_LIMIT = 100
+    CTR_ENTITIES_LIMIT = CTR_DEFAULT_ENTITIES_LIMIT
+
+    try:
+        limit = int(os.environ.get('CTR_ENTITIES_LIMIT'))
+        if limit > 0:
+            CTR_ENTITIES_LIMIT = limit
+    except (ValueError, TypeError):
+        pass
+
+    CTIM_SCHEMA_VERSION = '1.0.16'
+
+    CTIM_SIGHTING_DEFAULT = {
+        'type': 'sighting',
+        'schema_version': CTIM_SCHEMA_VERSION,
+        'source': URL_SCAN_SOURCE_NAME,
+        'confidence': 'High',
+        'internal': False,
+        'description': 'Scan Result'
+    }
--- tests/unit/api/test_enrich.py
+++ tests/unit/api/test_enrich.py
@@ -1,47 +1,168 @@
-# from http import HTTPStatus
-#
-# from pytest import fixture
-#
-# from .utils import headers
-#
-#
-# def routes():
-#     yield '/deliberate/observables'
-#     yield '/observe/observables'
-#     yield '/refer/observables'
-#
-#
-# @fixture(scope='module', params=routes(), ids=lambda route: f'POST {route}')
-# def route(request):
-#     return request.param
-#
-#
-# def test_enrich_call_with_invalid_jwt_failure(route, client, invalid_jwt):
-#     response = client.post(route, headers=headers(invalid_jwt))
-#     assert response.status_code == HTTPStatus.FORBIDDEN
-#
-#
-# @fixture(scope='module')
-# def invalid_json():
-#     return [{'type': 'unknown', 'value': ''}]
-#
-#
-# def test_enrich_call_with_valid_jwt_but_invalid_json_failure(route,
-#                                                              client,
-#                                                              valid_jwt,
-#                                                              invalid_json):
-#     response = client.post(route,
-#                            headers=headers(valid_jwt),
-#                            json=invalid_json)
-#     assert response.status_code == HTTPStatus.BAD_REQUEST
-#
-#
-# @fixture(scope='module')
-# def valid_json():
-#     return [{'type': 'domain', 'value': 'cisco.com'}]
-#
-#
-# def test_enrich_call_success(route, client, valid_jwt, valid_json):
-#     response = client.post(
-#     route, headers=headers(valid_jwt), json=valid_json)
-#     assert response.status_code == HTTPStatus.OK
+from http import HTTPStatus
+
+from pytest import fixture
+from unittest import mock
+
+from .utils import headers
+from tests.unit.mock_for_tests import (
+    EXPECTED_RESPONSE_404_ERROR,
+    EXPECTED_RESPONSE_500_ERROR,
+    EXPECTED_RESPONSE_AUTH_ERROR,
+    EXPECTED_RESPONSE_429_ERROR,
+    SEARCH_RESPONSE_MOCK,
+    EXPECTED_SUCCESS_RESPONSE
+)
+
+
+def routes():
+    yield '/observe/observables'
+
+
+@fixture(scope='module', params=routes(), ids=lambda route: f'POST {route}')
+def route(request):
+    return request.param
+
+
+@fixture(scope='function')
+def url_scan_api_request():
+    with mock.patch('requests.get') as mock_request:
+        yield mock_request
+
+
+def url_scan_api_response(*, ok, status_error=None):
+    mock_response = mock.MagicMock()
+
+    mock_response.ok = ok
+
+    if ok:
+        payload = SEARCH_RESPONSE_MOCK
+
+    else:
+        mock_response.status_code = status_error
+
+    mock_response.json = lambda: payload
+
+    return mock_response
+
+
+@fixture(scope='module')
+def invalid_json():
+    return [{'type': 'unknown', 'value': ''}]
+
+
+def test_enrich_call_with_invalid_json_failure(route, client, valid_jwt,
+                                               invalid_json):
+    response = client.post(
+        route, headers=headers(valid_jwt), json=invalid_json)
+
+    expected_payload = {
+        'errors': [
+            {
+                'code': 'invalid argument',
+                'message': mock.ANY,
+                'type': 'fatal',
+            }
+        ]
+    }
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.get_json() == expected_payload
+
+
+@fixture(scope='module')
+def valid_json():
+    return [{'type': 'ip', 'value': '1.1.1.1'}]
+
+
+@fixture(scope='module')
+def valid_json_multiple():
+    return [
+        {'type': 'ip', 'value': '1.1.1.1'},
+        {'type': 'ip', 'value': '2.2.2.2'},
+    ]
+
+
+def test_enrich_call_success(route, client, valid_jwt, valid_json,
+                             url_scan_api_request):
+    url_scan_api_request.return_value = url_scan_api_response(ok=True)
+
+    response = client.post(route, headers=headers(valid_jwt), json=valid_json)
+
+    assert response.status_code == HTTPStatus.OK
+
+    data = response.get_json()
+
+    assert data['data']['sightings']['docs'][0].pop('id')
+    assert data['data']['sightings']['docs'][1].pop('id')
+
+    assert data == EXPECTED_SUCCESS_RESPONSE
+
+
+def test_enrich_error_with_data(route, client, valid_jwt, valid_json_multiple,
+                                url_scan_api_request):
+
+    url_scan_api_request.side_effect = (
+        url_scan_api_response(ok=True),
+        url_scan_api_response(
+            ok=False, status_error=HTTPStatus.TOO_MANY_REQUESTS),
+    )
+
+    response = client.post(route, headers=headers(valid_jwt),
+                           json=valid_json_multiple)
+
+    assert response.status_code == HTTPStatus.OK
+
+    data = response.get_json()
+
+    assert data['data']['sightings']['docs'][0].pop('id')
+    assert data['data']['sightings']['docs'][1].pop('id')
+
+    expected_data = {}
+    expected_data.update(EXPECTED_SUCCESS_RESPONSE)
+    expected_data.update(EXPECTED_RESPONSE_429_ERROR)
+
+    assert data == expected_data
+
+
+def test_health_call_auth_error(route, client, valid_jwt, valid_json,
+                                url_scan_api_request):
+    url_scan_api_request.return_value = url_scan_api_response(
+        ok=False, status_error=HTTPStatus.UNAUTHORIZED)
+
+    response = client.post(route, headers=headers(valid_jwt), json=valid_json)
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.get_json() == EXPECTED_RESPONSE_AUTH_ERROR
+
+
+def test_health_call_404(route, client, valid_jwt, valid_json,
+                         url_scan_api_request):
+    url_scan_api_request.return_value = url_scan_api_response(
+        ok=False, status_error=HTTPStatus.NOT_FOUND)
+
+    response = client.post(route, headers=headers(valid_jwt), json=valid_json)
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.get_json() == EXPECTED_RESPONSE_404_ERROR
+
+
+def test_health_call_500(route, client, valid_jwt, valid_json,
+                         url_scan_api_request):
+    url_scan_api_request.return_value = url_scan_api_response(
+        ok=False, status_error=HTTPStatus.INTERNAL_SERVER_ERROR)
+
+    response = client.post(route, headers=headers(valid_jwt), json=valid_json)
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.get_json() == EXPECTED_RESPONSE_500_ERROR
+
+
+def test_health_call_429(route, client, valid_jwt, valid_json,
+                         url_scan_api_request):
+    url_scan_api_request.return_value = url_scan_api_response(
+        ok=False, status_error=HTTPStatus.TOO_MANY_REQUESTS)
+
+    response = client.post(route, headers=headers(valid_jwt), json=valid_json)
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.get_json() == EXPECTED_RESPONSE_429_ERROR
