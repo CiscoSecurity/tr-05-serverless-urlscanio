e454c5941ab1ff71631b58e63457e9be0d7b3c45
Heorhii Yatsenko <gosha1104@gmail.com>
2020-07-01 11:26:19 +0300

    removed duplicate observable_types, fixed error with ipv6 and url search (#9)

--- api/health.py
+++ api/health.py
@@ -12,7 +12,8 @@ def health():
     client = URLScanClient(
         base_url=current_app.config['URL_SCAN_API_URL'],
         api_key=api_key,
-        user_agent=current_app.config['USER_AGENT']
+        user_agent=current_app.config['USER_AGENT'],
+        observable_types=current_app.config['URL_SCAN_OBSERVABLE_TYPES']
     )
     client.get_search_data(
         current_app.config['URL_SCAN_HEALTH_CHECK_OBSERVABLE'])
