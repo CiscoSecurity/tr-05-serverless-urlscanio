f8936c6f2fe389dc9fdc629ac434abf443f4fa9c
mistergreen22 <44807224+mistergreen22@users.noreply.github.com>
2020-09-08 14:41:07 +0300

    Test refer (#25)

--- tests/functional/tests/constants.py
+++ tests/functional/tests/constants.py
@@ -1,2 +1,5 @@
 MODULE_NAME = 'urlscan.io'
 CONFIDENCE_LEVEL = SEVERITY_LEVEL = 'High'
+URL = 'https://urlscan.io'
+RELATION_TYPES = ['Contains', 'Resolved_To', 'Hosted_By']
+CTR_ENTITIES_LIMIT = 100
